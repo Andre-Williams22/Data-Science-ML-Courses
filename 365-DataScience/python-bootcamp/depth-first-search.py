8b86cb32b611909b04e46a3654be9125621d31e0
andre <awjrs22@gmail.com>
2020-09-04 23:29:03 -0400

    finish depth first search

---
+++ 365-DataScience/python-bootcamp/depth-first-search.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, name): 
+        self.name = name
+        self.children = []
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self 
+
+    def depthFirstSearch(self, array):
+        # call the first node: A
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+
+        return array 
+
